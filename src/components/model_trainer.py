b8e6bd53b5cee93a9698582f7a9a56b3ef18d193
saakoclh <staskolchin5@gmail.com>
2024-06-11 21:26:00 +0300

    Model

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,1 +1,21 @@
- 
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+
+from scr.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
